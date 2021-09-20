8d69d942b280ca93287e1b01abb687b32a89f69f
Houssam Eddine Aziez <houssamaziez@gmail.com>
2021-09-20 02:00:24 +0200

    Add Home Navigation Bar

---
+++ lib/Controller/controllerhome.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:get/get.dart';
+
+class ControllerHome extends GetxController {
+  var indextnavigator = 1.obs;
+  final drawerController = ZoomDrawerController();
+
+  navigIndext(indext) {
+    indextnavigator.value = indext;
+    update();
+  }
+}
--- lib/View/Home/home_view.dart
+++ lib/View/Home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:upprog/View/widgets/navigation_bar_widget.dart';
 
 class HomeView extends StatelessWidget {
   // final ZoomDrawerController drawercontroller;
@@ -8,22 +9,8 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Expanded(
-            child: Container(
-                width: MediaQuery.of(context).size.width,
-                child: Center(
-                  child: DefaultTextStyle(
-                    style: const TextStyle(fontSize: 20.0, color: Colors.black),
-                    child: AnimatedTextKit(
-                      repeatForever: true,
-                      animatedTexts: [
-                        WavyAnimatedText('Hello World'),
-                      ],
-                      isRepeatingAnimation: true,
-                    ),
-                  ),
-                )))
+      children: const [
+        NavifationBar(),
       ],
     );
   }
